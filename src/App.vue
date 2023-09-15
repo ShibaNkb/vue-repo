a90e94ab4b8d8fe0920cc09d21374ba679d95725
saba <shiba.nkb@gmail.com>
2023-09-16 00:26:44 +0400

    Learning vue cli service
    Tool command

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>My cool vue app</h1>
+    <button>Add task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style></style>
