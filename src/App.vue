a40d68f4afde088c01e4cf27fe5556cf1c81bb84
saba <shiba.nkb@gmail.com>
2023-09-20 02:27:55 +0400

    custom components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>
-    <h1>My Cool Vue App</h1>
+  <div class="container">
+    <Header />
     <button>Add task</button>
   </div>
 </template>
 
 <script>
+import Header from "./Header.vue";
 export default {
   name: "App",
+  components: {
+    Header,
+  },
 };
 </script>
 
-<style></style>
+<style>
+#app {
+  text-align: center;
+  color: yellowgreen;
+  margin-top: 60px;
+}
+.container {
+  border: 1px solid blue;
+  padding: 20px;
+  max-width: 500px;
+  margin: auto;
+}
+button {
+  padding: 10px 15px;
+  background-color: darkgreen;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
